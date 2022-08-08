d3f0ecf5633c9bc07b99344c3ce748404241b977
barispe <perveroglubaris@gmail.com>
2022-08-08 18:31:27 +0300

    finalize project

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player"){
-        Debug.Log("You Finished!");
-    }
-    }
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
